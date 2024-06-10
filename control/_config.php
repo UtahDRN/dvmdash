ed73f5c99efbd97fcbd8e4d64727cf05313dc36c
candrist <elderandrist@hotmail.com>
2024-06-10 09:59:02 -0600

    Update _config.php

--- control/_config.php
+++ control/_config.php
@@ -1,8 +1,8 @@
 <?php
-        $vcconfig = '/opt/centrunk/configs/configVC1.yml';
-        $ccconfig = '/opt/centrunk/configs/configCC1.yml';
-        $dvconfig = '/opt/centrunk/configs/configDVRS1.yml';
-        $ridacl = '/opt/centrunk/configs/rid_acl.dat';
+        $vcconfig = '/etc/dvmhost/configs/utahdrn-vc1.yml';
+        $ccconfig = '/etc/dvmhost/configs/utahdrn-cc1.yml';
+        $dvconfig = '/etc/dvmhost/configs/utahdrn-dvrs1.yml';
+        $ridacl = '/etc/dvmhost/configs/rid_acl.dat';
 
         $apihost = 'monitor.centrunk.net';
         $apikey = 'XXXXXXXXXXXXXXXXXXXXX';
@@ -11,12 +11,12 @@
         $username = 'admin';
         $password = 'password123!';
 
-        $logDir = '/var/log/centrunk/';
+        $logDir = '/var/log/dvmhost/';
         $showLocation = 'false';
         $showIdAlias = 'false';
         $showTgAlias = 'false';
         $defaultTimeZone = 'true';
-        $customTimeZone = "America/New_York";
+        $customTimeZone = "America/Denver;
         $fontsize = '28px';
         
 ?>
